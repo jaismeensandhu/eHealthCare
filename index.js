f38e30c6ad0bcb7080c04636d60467fdd1ddb949
Jaismeen Sandhu <jaismeen.sandhu@yahoo.com>
2016-11-21 00:19:43 -0500

    Database

--- index.js
+++ index.js
@@ -1,157 +1,48 @@
-var SERVER_NAME = 'user-api'
-var PORT = (process.env.PORT || 5000);
-var HOST = '127.0.0.1';
+var express = require('express');
+var cool = require('cool-ascii-faces');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM cities', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 
-var restify = require('restify')
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
-  // Get a persistence engine for the users
-  , usersSave = require('save')('users')
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 
-  // Create the restify server
-  , server = restify.createServer({ name: SERVER_NAME})
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
 
-  server.listen(PORT, function () {
-  console.log('Server %s listening at %s', server.name, server.url)
-  console.log('Resources:')
-  console.log(' /users')
-  console.log(' /users/:id')  
-})
-
-server
-  // Allow the use of POST
-  .use(restify.fullResponse())
-
-  // Maps req.body to req.params so there is no switching between them
-  .use(restify.bodyParser())
-
-  
-// Get all users in the system
-server.get('/', function (req, res, next) {
-	
-	 var resourceInfo = {
-		_links: [
-			{
-				"rel": "users.get",
-				"href": "/users"
-			},
-			{
-				"rel": "users.add",
-				"href": "/users"
-			},
-			{
-				"rel": "users.get-by-id",
-				"href": "/users/:id"
-			}
-		]
-	}
-	
-    res.send(resourceInfo)
-})
-  
-  
-// Get all users in the system
-server.get('/users', function (req, res, next) {
-
-  // Find every entity within the given collection
-  usersSave.find({}, function (error, users) {
-
-    // Return all of the users in the system
-    res.send(users)
-  })
-})
-
-// Get a single user by their user id
-server.get('/users/:id', function (req, res, next) {
-
-  // Find a single user by their id within save
-  usersSave.findOne({ _id: req.params.id }, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-    if (user) {
-      // Send the user if no issues
-      res.send(user)
-    } else {
-      // Send 404 header if the user doesn't exist
-      res.send(404)
-    }
-  })
-})
-
-// Create a new user
-server.post('/users', function (req, res, next) {
-
-  // Make sure name is defined
-  if (req.params.name === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('name must be supplied'))
-  }
-  if (req.params.age === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('age must be supplied'))
-  }
-  var newUser = {
-		name: req.params.name, 
-		age: req.params.age
-	}
-
-  // Create the user using the persistence engine
-  usersSave.create( newUser, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-    // Send the user if no issues
-    res.send(201, user)
-  })
-})
-
-// Update a user by their id
-server.put('/users/:id', function (req, res, next) {
-
-  // Make sure name is defined
-  if (req.params.name === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('name must be supplied'))
-  }
-  if (req.params.age === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('age must be supplied'))
-  }
-  
-  var newUser = {
-		_id: req.params.id,
-		name: req.params.name, 
-		age: req.params.age
-	}
-  
-  // Update the user with the persistence engine
-  usersSave.update(newUser, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-    // Send a 200 OK response
-    res.send(200)
-  })
-})
-
-// Delete user with the given id
-server.del('/users/:id', function (req, res, next) {
-
-  // Delete the user with the persistence engine
-  usersSave.delete(req.params.id, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-    // Send a 200 OK response
-    res.send()
-  })
-})
-
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
